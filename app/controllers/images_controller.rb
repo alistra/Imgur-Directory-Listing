7c1b377a64405331467f2c4e7b507c2ad305b1a1
Aleksander Balicki <balicki.aleksander@gmail.com>
2010-09-21 23:03:55 +0200

    some view

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,6 @@
 class ImagesController < ApplicationController
   def index
-    @images = Image.paginate :page => params[:page], :order => 'created_at DESC', :per_page => 100
+    @images = Image.paginate :page => params[:page], :order => 'created_at DESC', :per_page => 40
   end
 
   def show
@@ -9,7 +9,8 @@ class ImagesController < ApplicationController
   end
 
   def search
-    @image = Image.first(:conditions => ["name like ?", params[:partial]+"%"])
+    images = Image.all(:conditions => ["name like ?", params[:partial]+"%"])
+    @image = images[rand(images.size)]
     redirect_to @image.imglink
   end
 end
--- app/views/images/index.rss.builder
+++ app/views/images/index.rss.builder
@@ -8,7 +8,7 @@ xml.rss :version => "2.0" do
     for image in @images
       xml.item do
         xml.title image.name
-        xml.description descriptify image.imglink
+        xml.description image_tag image.imglink
         xml.pubDate image.created_at.to_s(:rfc822)
         xml.link image_url(image)
         xml.guid image_url(image)
