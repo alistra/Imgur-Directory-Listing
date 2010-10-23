3a4096f79aa0b7821ab17b499697b0c449ae6f0d
Aleksander Balicki <balicki.aleksander@gmail.com>
2010-10-23 13:28:05 +0200

    incron fixes

--- config/initializers/read_dir.rb
+++ config/initializers/read_dir.rb
@@ -1,10 +1,1 @@
 ENV['DISABLE_INITIALIZER_FROM_RAKE'] || Image.read_dir
-
-=begin require 'rb-inotify'
-  
-  notifier = INotify::Notifier.new
-  notifier.watch("/srv/www/", :moved_to, :create, :delete, :moved_from) do
-    Image.read_dir
-  end
-  notifier.process
-=end
